2591f22a02ee8c48f430498dc1b27a742201033d
Aditya Ladwa <ladwa.aditya@gmail.com>
2017-03-13 15:29:44 +0530

    Fect(EditNote): Added Edit Note activity

---
+++ app/src/main/java/com/ladwa/aditya/notehomelane/ui/edit/EditNoteActivity.java
@@ -0,0 +1,15 @@
+package com.ladwa.aditya.notehomelane.ui.edit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ladwa.aditya.notehomelane.R;
+
+public class EditNoteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_note);
+    }
+}
