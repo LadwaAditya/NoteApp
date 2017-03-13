8fab2eeacf6e4779dab81ee3259947eaaebb3f22
Aditya Ladwa <ladwa.aditya@gmail.com>
2017-03-13 15:30:59 +0530

    Fect(NoteDetail): Added Detail Activity for note detail

---
+++ app/src/main/java/com/ladwa/aditya/notehomelane/ui/detail/DetailActivity.java
@@ -0,0 +1,15 @@
+package com.ladwa.aditya.notehomelane.ui.detail;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ladwa.aditya.notehomelane.R;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
